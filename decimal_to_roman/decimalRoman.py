4cdd2a0748e4e24216f00a9ccfcccea0c00cf8ec
Azeez <azeezsayed1239@gmail.com>
2023-04-27 14:44:15 +0530

    change decimal to Roman for 1,2,3

--- decimal_to_roman/decimalRoman.py
+++ decimal_to_roman/decimalRoman.py
@@ -3,14 +3,16 @@ import unittest
 
 class Test_Decimal_Roman(unittest.TestCase):
 
-    def test_decimal_to_roman(self):
-        self.assertEqual(Decimal_Roman.decimal_to_roman(1), "I")
+    def test_decimal_to_rom(self):
+        self.assertEqual(Decimal_Roman.decimal_to_rom(1), "I"), print("yes")
+        self.assertEqual(Decimal_Roman.decimal_to_rom(2), "II"), print("yes")
+        self.assertEqual(Decimal_Roman.decimal_to_rom(3), "III"), print("yes")
 
 
 class Decimal_Roman():
-    def decimal_to_roman(dec):
-        if dec == 1:
-            return "I"
+    def decimal_to_rom(dec):
+        while dec <= 3 and dec > 0:
+            return "I"*dec
 
 
 if __name__ == '__main__':
