addb0adaede9558f8cd66df7f642c4cb81df9ac8
Azeez <azeezsayed1239@gmail.com>
2023-04-27 15:13:36 +0530

    change for all decimals to roman numerals

--- decimal_to_roman/decimalRoman.py
+++ decimal_to_roman/decimalRoman.py
@@ -13,11 +13,32 @@ class Test_Decimal_Roman(unittest.TestCase):
                          "VIII"), print("pass")
         self.assertEqual(Decimal_Roman.decimal_to_rom(9), "IX"), print("pass")
         self.assertEqual(Decimal_Roman.decimal_to_rom(10), "X"), print("pass")
+        self.assertEqual(Decimal_Roman.decimal_to_rom(125),
+                         "CXXV"), print("pass")
+        self.assertEqual(Decimal_Roman.decimal_to_rom(400),
+                         "CD"), print("pass")
+        self.assertEqual(Decimal_Roman.decimal_to_rom(500), "D"), print("pass")
+        self.assertEqual(Decimal_Roman.decimal_to_rom(600),
+                         "DC"), print("pass")
+        self.assertEqual(Decimal_Roman.decimal_to_rom(900),
+                         "CM"), print("pass")
+        self.assertEqual(Decimal_Roman.decimal_to_rom(1000),
+                         "M"), print("pass")
+        self.assertEqual(Decimal_Roman.decimal_to_rom(1234),
+                         "MCCXXXIV"), print("pass")
 
 
 class Decimal_Roman():
 
     ROMAN_NUMERAL = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
         10: 'X',
         9: 'IX',
         5: 'V',
@@ -26,12 +47,12 @@ class Decimal_Roman():
     }
 
     def decimal_to_rom(dec):
-        roman_numeral = ''
+        roman = ''
         for value, numeral in Decimal_Roman.ROMAN_NUMERAL.items():
             while dec >= value:
-                roman_numeral += numeral
+                roman += numeral
                 dec -= value
-        return roman_numeral
+        return roman
 
 
 if __name__ == '__main__':
